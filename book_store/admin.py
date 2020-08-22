ec0bbb228534269815c3b1300beb841ced0799d2
SamHo <sam.ho@kdanmobile.com>
2020-08-22 14:40:01 +0800

    Add OpeningHour in admin and Add list_bookstores unfinished sample

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import BookStore, Book, BookInBookStore
+from .models import BookStore, Book, BookInBookStore, OpeningHour
 
 # Register your models here.
 admin.site.register(BookStore)
 admin.site.register(Book)
 admin.site.register(BookInBookStore)
+admin.site.register(OpeningHour)
